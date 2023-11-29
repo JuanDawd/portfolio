3fccb5fa60591e5c2bad2e1e4f250f28680e8985
Juank Dawd <JuanDawdB@gmail.com>
2023-11-29 15:18:42 -0500

    fix: :ambulance: path fix

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
 
 const nextConfig = {
-  webpack: config => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './'),
-    }
-
-    return config
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
