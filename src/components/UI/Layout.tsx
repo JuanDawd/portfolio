d4bb4b93a790ee23781228c2ce8fc4cad238149a
Juank Dawd <JuanDawdB@gmail.com>
2024-02-01 00:54:49 -0500

    style: :lipstick: change padding for a screen size

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({ children, className = '' }: LayoutProps) => {
   return (
     <main className='mx-auto flex w-full max-w-[1200px] flex-col items-center justify-center'>
       <div
-        className={`z-0 inline-block h-full w-full max-w-[1200px] bg-background p-24 xl:p-16 lg:p-8 md:p-6 sm:p-4 ${className}`}
+        className={`z-0 inline-block h-full w-full max-w-[1200px] bg-background py-24 xl:p-16 lg:p-8 md:p-6 sm:p-4 ${className}`}
       >
         {children}
       </div>
